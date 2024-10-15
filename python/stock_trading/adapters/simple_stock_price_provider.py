c9ec9ed55e9f693456bd7c5757431a327fa51849
zkan <zkan.cs@gmail.com>
2024-10-15 11:40:29 +0700

    Randomize price

--- python/stock_trading/adapters/simple_stock_price_provider.py
+++ python/stock_trading/adapters/simple_stock_price_provider.py
@@ -1,6 +1,9 @@
+import random
+
 from ports.ports import StockPriceProvider
 
 
 class SimpleStockPriceProvider(StockPriceProvider):
     def get_price(self, stock_symbol):
-        return 100.0
+        price = random.randint(100, 500)
+        return price
